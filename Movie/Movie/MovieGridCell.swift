02621ab33ff0e8278520eaf68a224d359fb80511
FChadni <begumfa1@msu.edu>
2021-09-18 23:50:06 -0400

    second commit

---
+++ Movie/Movie/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Movie
+//
+//  Created by Farjana Chadni on 9/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+   
+    @IBOutlet var posterView: UIImageView!
+}
